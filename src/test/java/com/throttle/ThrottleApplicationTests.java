8cbc0d95146385552bf4d5aeab4f5245e4ba115b
Jigyasa Srivastava <connectjigyasasrivastava@gmail.com>
2026-06-26 23:23:45 +0530

    chore: scaffold Spring Boot project with web, redis, actuator, lombok

---
+++ src/test/java/com/throttle/ThrottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.throttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrottleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
